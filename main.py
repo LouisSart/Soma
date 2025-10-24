8aa5cdf6b793cc3c31fe1bb8abc4976dd6a785bc
Louis Sarthou <louissarthou@gmail.com>
2025-10-24 19:30:22 +0200

    previous error fixed

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def assemble(grid, p_ind, pieces, previous_calls):
         for pos in all_positions[p]:
             if fits(grid, pos):
                 if p_ind == len(pieces) - 1:
+                    previous_calls.add((grid, p_ind))
                     return 1
                 else:
                     ret += assemble(grid + pos, p_ind + 1, pieces, previous_calls)
-    else:
         previous_calls.add((grid, p_ind))
     return ret
 
-print("Soma cube solution found :", assemble(0, 0, ["W", "Nb", "Na", "T", "S", "L", "l"], set()))
+print("Soma cube solutions found :", assemble(0, 0, ["W", "Nb", "Na", "T", "S", "L", "l"], set()))
